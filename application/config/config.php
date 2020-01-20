38ace0ea53292543f988472d93e7fb1aaf177ec9
Fiqar Aprialim <fiqarapr@gmail.com>
2020-01-20 23:53:34 +0800

    No changes, random mod (transisi linux ke windows)

--- application/config/config.php
+++ application/config/config.php
@@ -32,7 +32,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ft-uh-scheduler/';
+// $config['base_url'] = 'http://localhost/ft-uh-scheduler/';
+// Dynamic & Clean Base Url
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http"); // Decide Http or Https
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST']; // Add Server host name for like Apache, Nginx or LiteSpeed.
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']); // add script name
+
 
 /*
 |--------------------------------------------------------------------------
