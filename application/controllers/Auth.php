e5b660ae5b24f9553e58b1214a952b10fa5b61b7
Fiqar Aprialim <fiqarapr@gmail.com>
2020-03-16 22:37:04 +0800

    Push subscription & notification seminar dalam controller auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,17 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+use Minishlink\WebPush\WebPush;
+use Minishlink\WebPush\Subscription;
 
 class Auth extends CI_Controller {
 
     public function __construct() {
-        parent::__construct();
-        date_default_timezone_set('Asia/Makassar');
+      parent::__construct();
+      date_default_timezone_set('Asia/Makassar');
 
-        $this->load->model('crud');
+      $this->load->model('crud');
     }
 
     public function index() {
-         $this->load->view('auth');
+      $this->load->view('auth');
     }
 
     public function subscription_check() {
@@ -20,12 +22,128 @@ class Auth extends CI_Controller {
               AS subscription FROM `subscription` WHERE username = '$username'")->result();
       echo json_encode($count);
     }
+    
+    public function push_subscription() {
+      $subscription = json_decode(file_get_contents('php://input'), true);
+
+      if (!isset($subscription['endpoint'])) {
+        echo 'Error: not a subscription';
+        return;
+      }
+
+      $method = $_SERVER['REQUEST_METHOD'];
+
+      switch ($method) {
+        case 'POST':
+          // create
+          $sub_insert = $this->db->query("INSERT INTO subscription (username, endpoint, p256dh, auth)
+            VALUES ('$subscription[username]', '$subscription[endpoint]', '$subscription[publicKey]', '$subscription[authToken]')");
+          echo "New record created";
+          break;
+        case 'PUT':
+          // update
+          $sub_select = $this->db->query("SELECT * FROM subscription WHERE username='$subscription[username]'")->result();
+          if ($sub_select[0]->username == $subscription['username'] && $sub_select[0]->endpoint == $subscription['endpoint'] 
+            && $sub_select[0]->p256dh == $subscription['publicKey'] && $sub_select[0]->auth == $subscription['authToken']) {
+              echo "No changes on record";
+              break;
+          } else {
+            $sub_update = $this->db->query("UPDATE subscription SET endpoint = '$subscription[endpoint]', 
+              p256dh = '$subscription[publicKey]', auth = '$subscription[authToken]' WHERE username = '$subscription[username]'");
+            echo "Record updated";
+            break;
+          };
+        default:
+          echo "Error: method not handled";
+          return;
+      };
+    }
+
+    public function push_notification_seminar() {
+      $input = json_decode(file_get_contents('php://input'), true);
+
+      $query_username = $this->db->query("SELECT nip FROM dosen WHERE nama_dosen='$input[nama_dosen]'")->result();
+      $sub_username = $query_username[0]->nip;
+      $query_subscription = $this->db->query("SELECT * FROM subscription WHERE username='$sub_username'")->result();
+
+      $nim_mahasiswa = $input['nim_mahasiswa'];
+      $nama_mahasiswa = $input['nama_mahasiswa'];
+      $ujian_tanggal = $input['ujian_tanggal'];
+      $ujian_waktu = $input['ujian_waktu'];
+      $ujian_tempat = $input['ujian_tempat'];
+
+      if ($input['jenis'] == 'konfirmasi_seminar_tutup') {
+          $title = "Konfirmasi Jadwal Ujian Skripsi Baru";
+          $body = "Nama : $nama_mahasiswa ($nim_mahasiswa)\nPada Tanggal $ujian_tanggal di $ujian_tempat.\nWaktu : $ujian_waktu.";
+      }
+      elseif ($input['jenis'] == 'konfirmasi_seminar_hasil') {
+          $title = "Konfirmasi Jadwal Seminar Hasil Baru";
+          $body = "Nama : $nama_mahasiswa ($nim_mahasiswa)\nPada Tanggal $ujian_tanggal di $ujian_tempat.\nWaktu : $ujian_waktu.";
+      } 
+      else {
+          $title = "No message";
+          $body = "No message";
+      }
+
+      echo $query_subscription[0]->username;
+
+      $auth = [
+          'GCM' => '267660624424', // deprecated and optional, it's here only for compatibility reasons
+          'VAPID' => [
+              'subject' => 'mailto:me@website.com', // can be a mailto: or your website address
+              'publicKey' => 'BBM3ZhbZ9j0Og57QieQ0dT6MjU5U4sVZcsc5j4dSWTlC3WvFp3Db1GBvwNcyAFfRn9VpiTuYUgcoFDSJYFYGkvo', // (recommended) uncompressed public key P-256 encoded in Base64-URL
+              'privateKey' => 'MmNVM1tOf4fgsF76QD-dJYL0iDM3rXBUOdKlZx8JWAo', // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
+          ],
+      ];
+
+      // array of notifications
+      $notifications = [
+          [
+              'subscription' => Subscription::create([ // this is the structure for the working draft from october 2018 (https://www.w3.org/TR/2018/WD-push-api-20181026/) 
+                  "endpoint" => $query_subscription[0]->endpoint,
+                  "keys" => [
+                      'p256dh' => $query_subscription[0]->p256dh,
+                      'auth' => $query_subscription[0]->auth
+                  ],
+              ]),
+              'payload' => json_encode([
+                  'title' => $title,
+                  'body' => $body,
+                  'url' => $input['url']
+              ]),
+          ],
+      ];
+
+      $webPush = new WebPush($auth);
+
+      // send multiple notifications with payload
+      foreach ($notifications as $notification) {
+          $webPush->sendNotification(
+              $notification['subscription'],
+              $notification['payload']
+          );
+      }
+
+      /**
+       * Check sent results
+       * @var MessageSentReport $report
+       */
+      foreach ($webPush->flush() as $report) {
+          $endpoint = $report->getRequest()->getUri()->__toString();
+
+          if ($report->isSuccess()) {
+              echo "Message sent successfully for subscription {$endpoint}.";
+          } else {
+              echo "Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+          }
+      }
+    }
 
     public function cekLogin(){
-        $username = $this->input->post('username');
-        $cek_pass = $this->input->post('password');
-        $password = md5($cek_pass);
-        $this->AuthModel->login($username, $password);
+      $username = $this->input->post('username');
+      $cek_pass = $this->input->post('password');
+      $password = md5($cek_pass);
+      $this->AuthModel->login($username, $password);
     }
 
     public function profil_mahasiswa(){
